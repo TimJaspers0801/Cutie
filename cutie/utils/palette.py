49c837a49b652a4284d8000105d8428e7a309222
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2025-03-05 11:06:48 +0100

    initial commit

--- cutie/utils/palette.py
+++ cutie/utils/palette.py
@@ -11,86 +11,84 @@ color_palette = {
     1: (255, 255, 255),  # Tools/camera - White
     2: (0, 0, 255),      # Vein (major) - Blue
     3: (255, 0, 0),      # Artery (major) - Red
-    4: (0, 255, 0),      # Small intestine - Green
-    5: (0, 200, 100),    # Colon/rectum - Dark Green
-    6: (200, 150, 100),  # Abdominal wall - Beige
-    7: (255, 200, 100),  # Omentum - Light Orange
-    8: (180, 0, 0),      # Aorta - Dark Red
-    9: (0, 0, 180),      # Vena cava - Dark Blue
-    10: (150, 100, 50),  # Liver - Brown
-    11: (0, 255, 255),   # Cystic duct - Cyan
-    12: (0, 200, 255),   # Gallbladder - Teal
-    13: (0, 100, 255),   # Hepatic vein - Light Blue
-    14: (255, 150, 50),  # Hepatic ligament - Orange
-    15: (200, 150, 0),   # Hepatocystic triangle - Mustard
+    4: (255, 255, 0),  # Nerve (major) - Yellow
+    5: (0, 255, 0),      # Small intestine - Green
+    6: (0, 200, 100),    # Colon/rectum - Dark Green
+    7: (200, 150, 100),  # Abdominal wall - Beige
+    8: (255, 200, 100),  # Omentum - Light Orange
+    9: (180, 0, 0),      # Aorta - Dark Red
+    10: (0, 0, 180),      # Vena cava - Dark Blue
+    11: (150, 100, 50),  # Liver - Brown
+    12: (0, 255, 255),   # Cystic duct - Cyan
+    13: (0, 200, 255),   # Gallbladder - Teal
+    14: (0, 100, 255),   # Hepatic vein - Light Blue
+    15: (255, 150, 50),  # Hepatic ligament - Orange
     16: (255, 220, 200), # Cystic plate - Light Pink
-    17: (255, 255, 0),   # Nerve (major) - Yellow
-    18: (200, 0, 200),   # Spleen - Purple
-    19: (255, 0, 150),   # Uterus - Pink
-    20: (255, 100, 200), # Ovary - Light Pink
-    21: (200, 100, 255), # Oviduct - Lavender
-    22: (150, 0, 100),   # Prostate - Dark Purple
-    23: (255, 200, 255), # Urethra - Light Pink
-    24: (150, 100, 50),  # Ligated plexus - Brown
-    25: (200, 0, 150),   # Seminal vesicles - Magenta
-    26: (100, 100, 100), # Catheter - Gray
-    27: (255, 150, 255), # Bladder - Light Purple
-    28: (100, 200, 255), # Kidney - Light Blue
+    17: (200, 0, 200),   # Spleen - Purple
+    18: (255, 0, 150),   # Uterus - Pink
+    19: (255, 100, 200), # Ovary - Light Pink
+    20: (200, 100, 255), # Oviduct - Lavender
+    21: (150, 0, 100),   # Prostate - Dark Purple
+    22: (255, 200, 255), # Urethra - Light Pink
+    23: (150, 100, 50),  # Ligated plexus - Brown
+    24: (200, 0, 150),   # Seminal vesicles - Magenta
+    25: (100, 100, 100), # Catheter - Gray
+    26: (255, 150, 255), # Bladder - Light Purple
+    27: (100, 200, 255), # Kidney - Light Blue
 
     # Thorax IDs
-    29: (150, 200, 255), # Lung - Light Blue
-    30: (0, 150, 255),   # Airway (bronchus/trachea) - Sky Blue
-    31: (255, 100, 100), # Esophagus - Salmon
-    32: (200, 200, 255), # Pericardium - Pale Blue
-    33: (100, 100, 255), # V azygos - Blue
-    34: (0, 255, 150),   # Thoracic duct - Green Cyan
-    35: (255, 255, 100), # Nerves - Yellow
+    28: (150, 200, 255), # Lung - Light Blue
+    29: (0, 150, 255),   # Airway (bronchus/trachea) - Sky Blue
+    30: (255, 100, 100), # Esophagus - Salmon
+    31: (200, 200, 255), # Pericardium - Pale Blue
+    32: (100, 100, 255), # V azygos - Blue
+    33: (0, 255, 150),   # Thoracic duct - Green Cyan
+    34: (255, 255, 100), # Nerves - Yellow
 
     # Non-anatomical structures
-    36: (150, 150, 150),  # Firefly - Gray
-    37: (50, 50, 50),    # Non anatomical structures - Dark Gray
-    38: (0, 0, 0),      # Excluded frames - Black
+    35: (150, 150, 150),  # Firefly - Gray
+    36: (50, 50, 50),    # Non anatomical structures - Dark Gray
+    37: (0, 0, 0),      # Excluded frames - Black
 }
 
 custom_names = {
     1: "Tools/camera",
     2: "Vein (major)",
     3: "Artery (major)",
-    4: "Small intestine",
-    5: "Colon/rectum",
-    6: "Abdominal wall",
-    7: "Omentum",
-    8: "Aorta",
-    9: "Vena cava",
-    10: "Liver",
-    11: "Cystic duct",
-    12: "Gallbladder",
-    13: "Hepatic vein",
-    14: "Hepatic ligament",
-    15: "Hepatocystic triangle",
+    4: "Nerve (major)",
+    5: "Small intestine",
+    6: "Colon/rectum",
+    7: "Abdominal wall",
+    8: "Omentum",
+    9: "Aorta",
+    10: "Vena cava",
+    11: "Liver",
+    12: "Cystic duct",
+    13: "Gallbladder",
+    14: "Hepatic vein",
+    15: "Hepatic ligament",
     16: "Cystic plate",
-    17: "Nerve (major)",
-    18: "Spleen",
-    19: "Uterus",
-    20: "Ovary",
-    21: "Oviduct",
-    22: "Prostate",
-    23: "Urethra",
-    24: "Ligated plexus",
-    25: "Seminal vesicles",
-    26: "Catheter",
-    27: "Bladder",
-    28: "Kidney",
-    29: "Lung",
-    30: "Airway (bronchus/trachea)",
-    31: "Esophagus",
-    32: "Pericardium",
-    33: "V azygos",
-    34: "Thoracic duct",
-    35: "Nerves",
-    36: "Firefly",
-    37: "Non anatomical structures",
-    38: "Excluded frames"
+    17: "Spleen",
+    18: "Uterus",
+    19: "Ovary",
+    20: "Oviduct",
+    21: "Prostate",
+    22: "Urethra",
+    23: "Ligated plexus",
+    24: "Seminal vesicles",
+    25: "Catheter",
+    26: "Bladder",
+    27: "Kidney",
+    28: "Lung",
+    29: "Airway (bronchus/trachea)",
+    30: "Esophagus",
+    31: "Pericardium",
+    32: "V azygos",
+    33: "Thoracic duct",
+    34: "Nerves",
+    35: "Firefly",
+    36: "Non anatomical structures",
+    37: "Excluded frames"
 }
 
 custom_palette_np = np.array([color_palette.get(i, (0, 0, 0)) for i in range(101)])
