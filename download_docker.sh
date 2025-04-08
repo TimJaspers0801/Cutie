e3a326c7140947ff131f089cb692c92bf3649821
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2025-04-08 13:09:10 +0200

    initial commit

--- download_docker.sh
+++ download_docker.sh
@@ -6,5 +6,10 @@
 #SBATCH --partition=gpu_a100
 #SBATCH --time=3:00:00
 
+# Set custom temporary directories (adjust the path if needed)
+export APPTAINER_TMPDIR=/gpfs/home1/tjaspers2/Cutie/temp/
+export APPTAINER_CACHEDIR=/gpfs/home1/tjaspers2/Cutie/temp/
+
+
 # Pull container from dockerhub
 apptainer pull container.sif docker://tjmjaspers/cutie:v1
