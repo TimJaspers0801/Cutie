e07e35a091be214eb07361248081b232ca5ad26e
TimJaspers <55140275+TimJaspers@users.noreply.github.com>
2025-04-08 13:13:29 +0200

    initial commit

--- download_docker.sh
+++ download_docker.sh
@@ -6,13 +6,16 @@
 #SBATCH --partition=gpu_a100
 #SBATCH --time=3:00:00
 
-# Set custom temporary directories (adjust the path if needed)
-export APPTAINER_TMPDIR=/gpfs/home1/tjaspers2/Cutie/temp/
-export APPTAINER_CACHEDIR=/gpfs/home1/tjaspers2/Cutie/temp/
 
-# Make sure the directory exists
-mkdir -p "$APPTAINER_TMPDIR"
-mkdir -p "$APPTAINER_CACHEDIR"
+# Set a custom path where you have more disk space (e.g., on /scratch)
+export MY_TMP_DIR=/gpfs/home1/tjaspers2/Cutie/temp/
+mkdir -p "$MY_TMP_DIR"
+
+# Set Apptainer/Singularity temp and cache directories
+export APPTAINER_TMPDIR="$MY_TMP_DIR"
+export APPTAINER_CACHEDIR="$MY_TMP_DIR"
+export TMPDIR="$MY_TMP_DIR"  # Some tools still use this
+
 
 # Pull container from dockerhub
 apptainer pull container.sif docker://tjmjaspers/cutie:v1
